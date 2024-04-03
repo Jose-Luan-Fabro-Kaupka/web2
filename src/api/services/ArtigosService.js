4a870cde693095cb8b57bcff25cb3261e2a93333
José Kaupka <104388981+Jose-Luan-Fabro-Kaupka@users.noreply.github.com>
2024-04-03 14:55:06 -0300

    Commit antes dos testes

--- src/api/services/ArtigosService.js
+++ src/api/services/ArtigosService.js
@@ -1,4 +1,4 @@
-const db = require('../config/db');
+const db = require('../../config/db');
 
 exports.artigosCriar = async (dados) => {
     const novoArtigo = await db.create(dados);
--- src/api/services/UsuariosService.js
+++ src/api/services/UsuariosService.js
@@ -1,4 +1,4 @@
-const db = require('../config/db');
+const db = require('../../config/db');
 
 exports.usuariosCriar = async (dados) => {
     const novousuario = await db.create(dados);
